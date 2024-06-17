cb996ada4162c902b1cef60235442d5c7ce92551
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-06-17 00:47:13 -0300

    refatorando

--- src/components/highlight.tsx
+++ src/components/highlight.tsx
@@ -4,7 +4,7 @@ import fotoPerfil from '../../public/perfil-1.png'
 
 export default function HighLight() {
   return (
-    <div className="flex flex-wrap-reverse  gap-10 items-center   justify-evenly  px-5">
+    <div className="flex flex-wrap  items-center   justify-evenly  px-5">
       <div className="flex flex-col font-bold  ">
         <h1 className="">Olá, eu sou o </h1>
         <p className="font-MsMadi nome  text-primary text-5xl max-w-[305px] ">
@@ -13,10 +13,6 @@ export default function HighLight() {
         <h2>Desenvolvedor Font-end</h2>
 
         <div className="mt-5  text-left max-w-[500px]">
-          <p>
-            Atualmente cursando 5º período de Anlálise e Desenvolvimento de
-            Sistemas, pela Universidade Estácio de Sá.
-          </p>
           <p>
             Especialzado na criação de sites resposivos, lojas virtuais e blogs.
             Tenho experiência com as mais recentes tecnologias de
@@ -37,13 +33,13 @@ export default function HighLight() {
         </Link>
       </div>
 
-      <div className=" z-10 dark:shadow-shadowdark shadow-shadowlight flex items-end h-[270px] w-[270px] lg:h-[400px] lg:w-[400px]  justify-center rounded-perfil border-2px border-green bg-bglightsecundary dark:bg-bgdarksecundary overflow-hidden ">
+      <div className=" z-0 md:z-10 relative -bottom-10 md:static  md:dark:shadow-shadowdark md:shadow-shadowlight flex items-end md:h-[270px] md:w-[270px] lg:h-[400px] lg:w-[400px]  justify-center md:rounded-perfil md:border-2px border-green md:dark:bg-bgdarksecundary overflow-hidden ">
         <Image
           src={fotoPerfil}
           height={400}
           width={400}
           alt=""
-          className="w-[250px] h-[250px] lg:h-[400px] lg:w-[400px] relative -bottom-5 hover:scale-105 transition-transform duration-500"
+          className="w-[350px] h-[350px] md:w-[250px] md:h-[250px]  lg:h-[400px] lg:w-[400px] relative -bottom-5 hover:scale-105 transition-transform duration-500"
         />
       </div>
     </div>

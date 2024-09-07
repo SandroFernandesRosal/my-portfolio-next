b6d8b445d0a21c6326519f82599f893d2cd85cf9
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-09-07 17:25:14 -0300

    refactor: removendo componentes não utilizados

--- src/components/background.tsx
+++
@@ -1,5 +0,0 @@
-export default function Background() {
-  return (
-    <div className="absolute right-[50%] top-0  h-[20%]  w-full -translate-y-1/2 translate-x-1/2 overflow-hidden rounded-full bg-primary px-28 py-16 opacity-50 blur-full" />
-  )
-}

6c3335d23afc30f311c14b54b75fec156f1e5053
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-09-07 15:04:48 -0300

    style: mudança no estilo

--- src/components/highlight.tsx
+++ src/components/highlight.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import fotoPerfil from '../../public/perfil-1.png'
+
 import { FaWhatsapp } from 'react-icons/fa'
 
 import ServiceHighLight from './service-higlight'
@@ -9,12 +8,36 @@ import { PiPersonArmsSpreadFill } from 'react-icons/pi'
 export default function HighLight() {
   return (
     <section className="flex flex-col relative">
-      <section className="flex flex-wrap mb-24  bg-[url(../assets/bgtopo-dark3.png)] dark:bg-[url(../assets/bgtopo-light3.png)]  bg-cover items-center pt-5  justify-center md:justify-around   bg-bglightsecundary dark:bg-bgdarksecundary  dark:shadow-none dark:border-b-[1px] dark:border-none">
-        <div className="flex flex-col mb-10 font-bold w-[100%]  md:max-w-[60%] lg:max-w-[50%]  z-20 ">
-          <h1 className="text-3xl text-center font-Rubiki md:text-4xl lg:text-5xl px-5  flex self-center">
+      <section className="flex flex-wrap mb-24 md:mb-36  bg-[url(../assets/bgtopo-dark3.png)] dark:bg-[url(../assets/bgtopo-light3.png)]  bg-cover items-center pt-5  justify-center md:justify-around   bg-bglightsecundary dark:bg-bgdarksecundary  dark:shadow-none dark:border-b-[1px] dark:border-none">
+        <div className="flex  justify-center   items-center mb-10 font-bold w-[100%]  md:max-w-[50%]  z-20 md:py-12">
+          <h1 className="text-3xl text-center  font-Rubiki md:text-4xl lg:text-5xl px-5 md:px-0    ">
             Transformo ideias em realidade digital, com soluções web inovadoras
             e personalizadas.
           </h1>
+        </div>
+
+        <div className="  mb-28 md:mb-0   flex flex-col items-center  justify-center  ">
+          <div className="flex justify-center gap-3 mt-5">
+            <div className="flex  items-center gap-1">
+              <span className="font-Rubiki text-5xl lg:text-7xl font-bold text-primary">
+                +3
+              </span>{' '}
+              <div className="flex flex-col text-sm">
+                <span className="lg:text-lg">anos de</span>
+                <span className="lg:text-lg">experiência</span>
+              </div>
+            </div>
+
+            <div className="flex  items-center gap-1">
+              <span className="font-bold text-5xl lg:text-7xl text-primary font-Rubiki">
+                +15
+              </span>{' '}
+              <div className="flex flex-col text-sm">
+                <span className="lg:text-lg">projetos</span>
+                <span className="lg:text-lg">concluídos</span>
+              </div>
+            </div>
+          </div>
 
           <div className="flex mt-5 gap-3 justify-center w-ful flex-wrap">
             <Link
@@ -36,16 +59,6 @@ export default function HighLight() {
               <FaWhatsapp className="text-2xl text-primary" />
             </Link>
           </div>
-        </div>
-
-        <div className="  mt-5    bg-cover md:bg-none dark:md:bg-none h-[315px] relative bottom-12 md:bottom-0 z-20  md:mb-[110px]  md:dark:shadow-shadowdark md:shadow-shadowlight md:flex items-end md:h-[270px] md:w-[270px] lg:h-[400px] lg:w-[400px]  justify-center md:rounded-perfil md:border-2px border-green md:dark:bg-bgdarksecundary md:bg-bglightsecundary overflow-hidden md:mt-5 ">
-          <Image
-            src={fotoPerfil}
-            height={400}
-            width={400}
-            alt=""
-            className="w-[350px] h-[350px] md:w-[250px] md:h-[250px]  lg:h-[400px] lg:w-[400px] relative -bottom-5 hover:scale-105 transition-transform duration-500"
-          />
         </div>
         <div className="h-[120px]  w-full bg-[url(../assets/bg-light.png)] dark:bg-[url(../assets/bg-dark.png)]  bg-right md:bg-left  bg-repeat-x" />
       </section>

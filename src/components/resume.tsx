d92b47c4013927477175bfa4a32b4949745a455b
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-09-07 15:31:10 -0300

    style: mudança de estilo

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -46,7 +46,7 @@ export default function Resume() {
           </div>
         </div>
 
-        <div className="text-left mx-5 mb-10 max-w-[600px]">
+        <div className="text-left mx-5 lg:mb-10 max-w-[600px]">
           <p className="pb-[100px]">
             Sempre fui fascinado pela tecnologia, mas foi em um momento muito
             difícil que descobri que a programação era o que eu realmente queria
@@ -59,7 +59,7 @@ export default function Resume() {
         </div>
       </div>
 
-      <div className=" h-[315px] md:mb-5 hidden md:dark:shadow-shadowdark md:shadow-shadowlight md:flex items-end md:h-[270px] md:w-[270px] lg:h-[400px] lg:w-[400px]  justify-center md:rounded-perfil md:border-2px border-green md:dark:bg-bgdarksecundary overflow-hidden md:mt-5">
+      <div className=" h-[325px] md:mb-5 md:dark:shadow-shadowdark md:shadow-shadowlight flex items-end md:h-[270px] md:w-[270px] lg:h-[400px] lg:w-[400px]  justify-center rounded-perfil border-2px border-green dark:bg-bgdarksecundary overflow-hidden md:mt-5">
         <Image
           src={fotoPerfil}
           height={400}

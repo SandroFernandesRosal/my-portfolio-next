8a561bfcf6ce308828b23a59759d11f0b843024c
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-11-01 01:21:37 -0300

    :ok_hand: alterações no estilo

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -31,7 +31,7 @@ export default function Project({
   return (
     <li
       key={id}
-      className={`max-w-[250px] dark:border-[1px] dark:border-zinc-800 project project-${id} flex flex-col   bg-bglightsecundary dark:bg-bgdarksecundary rounded-md shadow-shadowlight  dark:shadow-none hover:border-primary hover:border-[1px] border-transparent dark:hover:border-primary transition  delay-150 duration-300  ease-in-out hover:-translate-y-1 hover:scale-110  ${size === 'large' && 'w-[98%] max-w-[400px] h-[650px] justify-between'} ${size === 'small' && 'h-[230px] justify-between py-3 md:py-6 min-w-[100px] w-[25%] md:max-w-[150px] '} ${size === 'normal' && 'h-[500px] w-[45%] gap-2 md:gap-5 justify-between'}`}
+      className={`bg-[url(../assets/bg-dot.svg)]   max-w-[250px] dark:border-[1px] dark:border-zinc-800 project project-${id} flex flex-col   bg-bglightsecundary dark:bg-bgdarksecundary rounded-md shadow-shadowlight  dark:shadow-none hover:border-primary hover:border-[1px] border-transparent dark:hover:border-primary transition  delay-150 duration-300  ease-in-out hover:-translate-y-1 hover:scale-110  ${size === 'large' && 'w-[98%] max-w-[400px] h-[650px] justify-between'} ${size === 'small' && 'h-[230px] justify-between py-3 md:py-6 min-w-[100px] w-[25%] md:max-w-[150px] '} ${size === 'normal' && 'h-[500px] w-[45%] gap-2 md:gap-5 justify-between'}`}
     >
       <Image
         src={img}

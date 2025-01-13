761a18d83255f139eb056079ad6b98a47154da9a
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-01-13 01:15:15 -0300

    :ok_hand: style: alteração no estilo

--- src/components/skills-scroll.tsx
+++ src/components/skills-scroll.tsx
@@ -84,40 +84,40 @@ export default function SkillScroll() {
   return (
     <Slider
       {...settings}
-      className="slider-container flex self-center  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)] w-[100vw]  h-[170px]"
+      className="slider-container flex self-center  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)] w-[100vw]  h-[200px]"
     >
       {skillsData.map((skill: SkilsProps) => (
         <div
           key={skill.id}
-          className="p-2  flex flex-col items-center text-primary justify-center text-center h-[170px] justify-items-center content-center rounded-md  my-2"
+          className="p-2  flex flex-col items-center text-primary justify-center text-center w-[170px] h-[170px] justify-items-center content-center rounded-md  my-2"
         >
           {skill.name}
-          {skill.icon === 'HTML' && <RiHtml5Line className="text-9xl  " />}
-          {skill.icon === 'CSS' && <FaCss3Alt className="text-9xl " />}
-          {skill.icon === 'JS' && <TbBrandJavascript className="text-9xl " />}
-          {skill.icon === 'TS' && <TbBrandTypescript className="text-9xl " />}
-          {skill.icon === 'React' && <RiReactjsLine className="text-9xl " />}
-          {skill.icon === 'Next' && <RiNextjsLine className="text-9xl " />}
+          {skill.icon === 'HTML' && <RiHtml5Line className="text-8xl  " />}
+          {skill.icon === 'CSS' && <FaCss3Alt className="text-8xl " />}
+          {skill.icon === 'JS' && <TbBrandJavascript className="text-8xl " />}
+          {skill.icon === 'TS' && <TbBrandTypescript className="text-8xl " />}
+          {skill.icon === 'React' && <RiReactjsLine className="text-8xl " />}
+          {skill.icon === 'Next' && <RiNextjsLine className="text-8xl " />}
           {skill.icon === 'Tailwind' && (
-            <RiTailwindCssFill className="text-9xl " />
+            <RiTailwindCssFill className="text-8xl " />
           )}
           {skill.icon === 'Zustand' && (
-            <RiBearSmileLine className="text-9xl " />
+            <RiBearSmileLine className="text-8xl " />
           )}
           {skill.icon === 'Angular' && (
-            <RiAngularjsLine className="text-9xl " />
+            <RiAngularjsLine className="text-8xl " />
           )}
-          {skill.icon === 'Node' && <RiNodejsLine className="text-9xl " />}
+          {skill.icon === 'Node' && <RiNodejsLine className="text-8xl " />}
           {skill.icon === 'StyledComponents' && (
-            <SiStyledcomponents className="text-9xl " />
+            <SiStyledcomponents className="text-8xl " />
           )}
-          {skill.icon === 'Prisma' && <SiPrisma className="text-9xl " />}
-          {skill.icon === 'Git' && <FaGitAlt className="text-9xl " />}
-          {skill.icon === 'Github' && <FaGithub className="text-9xl " />}
-          {skill.icon === 'Db' && <FaDatabase className="text-9xl " />}
-          {skill.icon === 'Fastify' && <SiFastify className="text-9xl " />}
-          {skill.icon === 'Express' && <SiExpress className="text-9xl " />}
-          {skill.icon === 'Insomnia' && <SiInsomnia className="text-9xl " />}
+          {skill.icon === 'Prisma' && <SiPrisma className="text-8xl " />}
+          {skill.icon === 'Git' && <FaGitAlt className="text-8xl " />}
+          {skill.icon === 'Github' && <FaGithub className="text-8xl " />}
+          {skill.icon === 'Db' && <FaDatabase className="text-8xl " />}
+          {skill.icon === 'Fastify' && <SiFastify className="text-8xl " />}
+          {skill.icon === 'Express' && <SiExpress className="text-8xl " />}
+          {skill.icon === 'Insomnia' && <SiInsomnia className="text-8xl " />}
         </div>
       ))}
     </Slider>

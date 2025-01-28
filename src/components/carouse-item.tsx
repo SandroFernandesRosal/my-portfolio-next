09970868b1d54f27c4a47b9f181d2398bd0a0e5c
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-01-28 01:50:59 -0300

    :ok_hand: style: alteração

--- src/components/carouse-item.tsx
+++ src/components/carouse-item.tsx
@@ -5,14 +5,13 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import { ProjectArray, ProjectProps } from '@/data/types/projects'
 import { useSize, useDisplay, useSearch } from '@/store/useStore'
-import { useState } from 'react'
+
 import Project from './project'
 
 export default function CarouselItems({ projects }: ProjectArray) {
   const { display } = useDisplay()
   const { search } = useSearch()
   const { size } = useSize()
-  const [loading] = useState(false)
 
   const filteredProjects = projects.filter((project: ProjectProps) =>
     project.title.toLowerCase().includes(search.toLowerCase()),
@@ -94,31 +93,29 @@ export default function CarouselItems({ projects }: ProjectArray) {
 
   return (
     <div className="w-[90vw] lg:w-[70vw] pt-10 [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]">
-      {!loading ? (
-        displayedProjects.length > 0 ? (
-          <Slider {...settings}>
-            {displayedProjects.map((project: ProjectProps) => (
-              <Project
-                key={project.id}
-                id={project.id}
-                title={project.title}
-                img={project.img}
-                video={project.video}
-                repo={project.repo}
-                page={project.page}
-                tecs={project.tecs}
-                slug={project.slug}
-                imgs={project.imgs}
-                featured={project.featured}
-                description={project.description}
-              />
-            ))}
-          </Slider>
-        ) : (
-          <p>Nenhum projeto encontrado.</p>
-        )
+      {displayedProjects.length > 0 ? (
+        <Slider {...settings}>
+          {displayedProjects.map((project: ProjectProps) => (
+            <Project
+              key={project.id}
+              id={project.id}
+              title={project.title}
+              img={project.img}
+              video={project.video}
+              repo={project.repo}
+              page={project.page}
+              tecs={project.tecs}
+              slug={project.slug}
+              imgs={project.imgs}
+              featured={project.featured}
+              description={project.description}
+            />
+          ))}
+        </Slider>
       ) : (
-        <p>Carregando...</p>
+        <>
+          <p className="flex justify-center">Nenhum site encontrado.</p>
+        </>
       )}
     </div>
   )
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -11,7 +11,6 @@ export default function Project({ slug, title, img }: ProjectProps) {
       className={`border-[1px] dark:border-zinc-700 flex p-2 gap-1 rounded-md hover:border-primary hover:border-[1px] border-zinc-400 dark:hover:border-primary transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ${size === 'large' && 'w-[100%] h-[600px] justify-between'} ${size === 'normal' && 'h-[400px] flex-col md:h-[500px] w-[100%] justify-between'}`}
     >
       <Link
-        aria-label="abrir projeto"
         href={`/projetos/${slug}`}
         className="overflow-hidden rounded-md flex w-full h-full"
       >

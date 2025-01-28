0910afd750524cf0a1c44b25de1696d2250a0b42
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-01-27 23:13:01 -0300

    :ok_hand: style: alteração de tamanho padrão

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -16,7 +16,7 @@ export const useDataSearch = create((set) => ({
 }))
 
 export const useSize = create((set) => ({
-  size: 'large',
+  size: 'normal',
   setSize: (state) => set({ size: state }),
 }))
 

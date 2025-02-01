f3eaf5cc9cd5175c72252fb70b7dad921d535d02
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-02-01 19:01:32 -0300

    :ok_hand: style: correção

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import Image from 'next/image'
 import { ProjectProps } from '@/data/types/projects'
@@ -10,6 +11,8 @@ export default function Project({ slug, title, img }: ProjectProps) {
   return (
     <Link
       href={`/projetos/${slug}`}
+      aria-hidden="true"
+      tabIndex={-1}
       className={`border-[1px] dark:border-zinc-700 my-10   overflow-hidden  flex p-2 gap-1 rounded-md hover:border-primary hover:border-[1px] border-zinc-400 dark:hover:border-primary transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ${size === 'large' && 'w-[100%] h-[600px] justify-between'} ${size === 'normal' && 'h-[400px] flex-col md:h-[500px] w-[100%]  justify-between'}  ${search && size === 'large' && 'w-[80%] h-[600px] md:w-[100%] md:h-[800px]  md:max-w-[500px] lg:max-w-[400px] '} ${search && size === 'normal' && 'w-[45%] h-[400px] md:w-[300px]'} `}
     >
       <Image
